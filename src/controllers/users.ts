91e668cfc7887ab1be7389e5a9e0d4098ea4ede8
dyutelegin <dyutelegin@gmail.com>
2022-11-16 19:30:32 +0100

    add route getUserById

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -5,13 +5,25 @@ import { User } from "../entity/user";
 
 const userRepository = AppDataSource.getRepository(User);
 
-export const getAllusers = async (req: Request<{},{},{},{id: string}>, res: Response): Promise<void> => {
+export const getAllusers = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const result = await AppDataSource.manager.query(`
+      SELECT * from public.users`);
+
+    res.status(200).send({
+      result
+    })
+
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export const getUserById = async (req: Request<{},{},{},{id: string}>, res: Response): Promise<void> => {
   try {
     const{ id } = req.query
 
     const user = await userRepository.find({where: {id: parseInt(id)}});
-    const result = await AppDataSource.manager.query(`
-      SELECT * from public.users`);
 
     res.status(200).send({
       user
